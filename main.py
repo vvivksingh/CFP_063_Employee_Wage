0c12631d72d0b00e8034b634e2000ebf40bc5633
Vivek Singh <94423696+vvivksingh@users.noreply.github.com>
2022-02-21 12:21:31 +0530

    ability to calculate wage for multiple company

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import emp_wage
 
 if __name__ == '__main__':
-
-    print("Employee total wage is ", emp_wage.totalWage)
+    emp_wage.calculate_employee_wage(20, 20, 100, "Dmart")
+    emp_wage.calculate_employee_wage(30, 25, 110, "Jio")
 
 
