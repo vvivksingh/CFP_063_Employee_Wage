7e7b636c7c5b952a72f4f271325d3a7e4c1a9608
Vivek Singh <94423696+vvivksingh@users.noreply.github.com>
2022-02-21 14:09:07 +0530

    ability to manage wage for multiple company

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import emp_wage
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print("Welcome to Employee Wage Builder")
-    emp_dmart = emp_wage.EmployeeWageBuilder(20, 20, 100, "Dmart")
-    emp_jio = emp_wage.EmployeeWageBuilder(30, 25, 110, "Jio")
-    emp_dmart.calculate_employee_wage()
-    emp_jio.calculate_employee_wage()
-
+    emp_wage = emp_wage.EmployeeWageBuilder()
+    emp_wage.add_employee(20, 20, 100, "Dmart")
+    emp_wage.add_employee(30, 25, 110, "Jio")
+    emp_wage.calculate_employee_wage()
+    employees = "\n".join(str(employee) for employee in emp_wage.company_array)
+    print(employees)
 
