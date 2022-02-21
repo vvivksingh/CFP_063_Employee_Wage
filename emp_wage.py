8d5ea7252878c549862506bf6def22c85181b2f0
Vivek Singh <94423696+vvivksingh@users.noreply.github.com>
2022-02-21 10:49:17 +0530

    using dictionary to  store attandence

--- emp_wage.py
+++ emp_wage.py
@@ -1,10 +1,18 @@
 import random
-IS_PRESENT = 1
+IS_ABSENT = 0
+IS_PRESENT_FULL_DAY = 1
+IS_PRESENT_PART_TIME = 2
 WAGE_PER_HOUR = 20
 FULL_DAY_HOURS = 8
+PART_TIME_HOURS = 4
 
-emp_check = random.randint(0, 1)
-if emp_check == IS_PRESENT:
-    print(f"Employee is present and the wage is ₹ {WAGE_PER_HOUR * FULL_DAY_HOURS} ")
-else:
-    print("Employee is absent")
+emp_attandence = {
+    IS_PRESENT_FULL_DAY :
+    f"Employee is present for full time and the wage is {WAGE_PER_HOUR * FULL_DAY_HOURS}rs",
+    IS_PRESENT_PART_TIME:
+    f"Employee is present for part time and wage is {WAGE_PER_HOUR * PART_TIME_HOURS}rs",
+    IS_ABSENT:
+    f"Employee is absent"
+    }
+
+emp_check = random.randint(0, 2)
