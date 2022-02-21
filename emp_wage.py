8a4b4cb3f66a470c91f4401d5cfbdc1f00d5a81d
Vivek Singh <94423696+vvivksingh@users.noreply.github.com>
2022-02-21 10:21:25 +0530

    uc2 calculating daily wage if present

--- emp_wage.py
+++ emp_wage.py
@@ -1,8 +1,10 @@
 import random
-is_present = 1
+IS_PRESENT = 1
+WAGE_PER_HOUR = 20
+FULL_DAY_HOURS = 8
 
 emp_check = random.randint(0, 1)
-if emp_check == is_present:
-    print("Employee is present")
+if emp_check == IS_PRESENT:
+    print(f"Employee is present and the wage is ₹ {WAGE_PER_HOUR * FULL_DAY_HOURS} ")
 else:
     print("Employee is absent")
